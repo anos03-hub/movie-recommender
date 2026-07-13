69233da3151fd9e5046098fb096cc4b7e10a8d73
ap240203 <ap240203@gmail.com>
2026-07-13 12:54:30 +0300

    Nothing

--- recommend.py
+++ recommend.py
@@ -50,9 +50,7 @@ def recommend(selected_titles, top_n =5):
         if idx not in selected_idx:
             movie_id = idx_to_movieid[idx]
             titles = movies_titles[movies_titles['movieId'] == movie_id]['title'].values[0]
-            genres = movies_titles[movies_titles['movieId'] == movie_id]['genres'].values[0]
-            genres = genres.replace('|', ', ')
-            recommendations.append(f"{title} — {genres}")
+            recommendations.append(titles)
         if len(recommendations) == top_n:
             break
     return recommendations
